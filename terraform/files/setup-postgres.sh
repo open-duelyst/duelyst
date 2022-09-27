519235b2652b63bed00456e07470858512a813c3
Will Roberts <willroberts3@protonmail.com>
2022-09-27 14:34:34 -0400

    Removes Terraform for dev server (use Docker Compose)

--- terraform/files/setup-postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo su - postgres << EOF
-whoami
-createuser -w ubuntu
-psql -c "ALTER USER ubuntu WITH PASSWORD 'password';"
-createdb -O ubuntu duelyst
-EOF
--- terraform/run-postgres-tunnel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-HOST=$(terraform output -state "states/terraform_$BRANCH.tfstate" server_address)
-USER=root
-KEY=./ssh/terraform-deploy-branch-$BRANCH
-
-ssh -i $KEY -L 54322:localhost:5432 $USER@$HOST
