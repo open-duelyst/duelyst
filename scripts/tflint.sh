cdf4188a00c3b2c38998f9837d693a3d31eef734
Will Roberts <willroberts3@protonmail.com>
2022-09-28 13:42:24 -0400

    Adds terraform linting Action

---
+++ scripts/tflint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Lints Terraform code with tflint.
+for dir in $(find terraform | grep '\.tf$' | xargs dirname | sort -u); do
+	if [[ $dir == "terraform/modules"* ]]; then
+		# Ignore provider rules in modules.
+		tflint --disable-rule=terraform_required_providers \
+			--disable-rule=terraform_required_version $dir
+	else
+		tflint $dir
+	fi
+done
