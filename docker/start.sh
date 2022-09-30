ce4f08a295c628018618cb54b8c96713e0057216
Will Roberts <willroberts3@protonmail.com>
2022-09-30 18:41:21 -0400

    Switch localdev to node:16-alpine to match staging

--- docker/start.sh
+++ docker/start.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# Work around git vs https issue for old packages.
+# Work around boneskull/yargs dependency using the deprecated git protocol.
+apk add git
 git config --global url."https://github.com/".insteadOf git@github.com:
 git config --global url."https://".insteadOf git://
 
 # Install dependencies.
-yarn install --dev
+yarn install --production && yarn cache clean
 
 # Use exec to take over the PID from the shell, enabling signal handling.
 exec yarn $1
