4dcdba18e72823d8e3e1383980ae908089b7d6a3
Will Roberts <willroberts3@protonmail.com>
2022-09-30 17:16:39 -0400

    Simplifies ECR publisher

--- scripts/publish_container.sh
+++ scripts/publish_container.sh
@@ -13,8 +13,6 @@ VERSION=$2
 if [ -z $VERSION ]; then quit "Second argument must be VERSION e.g. '1.96.17'"; fi
 REGISTRY=$3
 if [ -z $REGISTRY ]; then quit "Third argument must be REGISTRY e.g. 'abcd1234'"; fi
-REPOSITORY=$4
-if [ -z $REPOSITORY ]; then quit "Fourth argument must be REPOSITORY e.g. 'my-repo-name'"; fi
 
 # Check AWS access.
 which aws > /dev/null || quit "AWS CLI is not installed. Exiting."
@@ -28,14 +26,14 @@ aws ecr-public get-login-password --region us-east-1 | \
 
 # Tag and publish the image (SemVer).
 LOCAL_IMAGE="duelyst-${SERVICE}:${VERSION}"
-REMOTE_IMAGE="public.ecr.aws/${REGISTRY}/${REPOSITORY}:${VERSION}"
+REMOTE_IMAGE="public.ecr.aws/${REGISTRY}/duelyst-${SERVICE}:${VERSION}"
 echo "Tagging $LOCAL_IMAGE as $REMOTE_IMAGE"
 docker tag $LOCAL_IMAGE $REMOTE_IMAGE
 echo "Pushing $REMOTE_IMAGE"
 docker push $REMOTE_IMAGE
 
 # Tag and publish the image (latest).
-REMOTE_IMAGE="public.ecr.aws/${REGISTRY}/${REPOSITORY}:latest"
+REMOTE_IMAGE="public.ecr.aws/${REGISTRY}/duelyst-${SERVICE}:latest"
 echo "Tagging $LOCAL_IMAGE as $REMOTE_IMAGE"
 docker tag $LOCAL_IMAGE $REMOTE_IMAGE
 echo "Pushing $REMOTE_IMAGE"
