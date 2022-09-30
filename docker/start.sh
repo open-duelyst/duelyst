f73dca0e3b106e05e20b6291abb1b4531a211d2f
Will Roberts <willroberts3@protonmail.com>
2022-09-30 19:07:54 -0400

    Fixes bcrypt in localdev

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,9 @@ apk add git
 git config --global url."https://github.com/".insteadOf git@github.com:
 git config --global url."https://".insteadOf git://
 
+# Install node-gyp dependencies for bcrypt in API and worker
+apk add python3 make gcc g++
+
 # Install dependencies.
 yarn install --production && yarn cache clean
 
