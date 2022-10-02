34d324757be4393f9902b908035ed94d25be1b9c
Will Roberts <willroberts3@protonmail.com>
2022-10-02 13:36:54 -0400

    Use TCP health checks for Socket.io

--- terraform/modules/load_balancer/main.tf
+++ terraform/modules/load_balancer/main.tf
@@ -49,7 +49,7 @@ resource "aws_lb_target_group" "api_target_group" {
 
   health_check {
     matcher = "200"
-    path    = "/" # FIXME: Use /healthcheck for API.
+    path    = "/healthcheck"
   }
 }
 
@@ -73,8 +73,7 @@ resource "aws_lb_target_group" "sp_target_group" {
   protocol         = "HTTP"
   protocol_version = "HTTP1"
 
-  #health_check {
-  #matcher = "200"
-  #  path = "/" # FIXME: Do we have an HTTP healthcheck?
-  #}
+  health_check {
+    protocol = "TCP"
+  }
 }
