e1ccea4f6a00346f613fe552ab981757dc9949bb
Will Roberts <willroberts3@protonmail.com>
2022-10-03 22:35:19 -0400

    Adds ElastiCache alarms

--- terraform/staging/alarms.tf
+++ terraform/staging/alarms.tf
@@ -8,3 +8,14 @@ module "billing_alarm" {
   threshold     = var.billing_alarm_threshold
   alarm_actions = [module.email_sns_topic.topic_arn]
 }
+
+module "redis_alarms" {
+  source               = "../modules/alarms/elasticache"
+  cluster_id           = "duelyst-staging"
+  node_id              = "0001"
+  instance_type        = module.redis.instance_type
+  cpu_threshold        = 90
+  engine_cpu_threshold = 90
+  memory_threshold     = 90
+  alarm_actions        = [module.email_sns_topic.topic_arn]
+}
