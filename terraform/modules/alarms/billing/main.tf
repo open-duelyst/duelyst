0688d5cd4fddd4a345476d2f41be05d27b18c148
Will Roberts <willroberts3@protonmail.com>
2022-10-03 20:12:18 -0400

    Fixes billing alarm

--- terraform/modules/alarms/billing/main.tf
+++ terraform/modules/alarms/billing/main.tf
@@ -2,13 +2,17 @@
 # Docs: https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/monitor_estimated_charges_with_cloudwatch.html#turning_on_billing_metrics
 resource "aws_cloudwatch_metric_alarm" "alarm" {
   alarm_name        = "estimated-billing"
-  alarm_description = "AWS bill is on track to exceed $${var.threshold} this month!"
+  alarm_description = "AWS bill is on track to exceed ${var.threshold} this month!"
 
-  namespace           = "Billing"
+  namespace           = "AWS/Billing"
   metric_name         = "EstimatedCharges"
-  statistic           = "Average"
+  statistic           = "Maximum"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   threshold           = var.threshold
+
+  dimensions = {
+    Currency = "USD"
+  }
 
   # Check the metric once per hour.
   # https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/AlarmThatSendsEmail.html#alarm-evaluation
