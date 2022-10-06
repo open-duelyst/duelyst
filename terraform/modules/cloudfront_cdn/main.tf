4dc320891939d1eb010b7bbd323638151f42f540
Will Roberts <willroberts3@protonmail.com>
2022-10-06 12:50:27 -0400

    Actually attach the response headers policy to CloudFront

--- terraform/modules/cloudfront_cdn/main.tf
+++ terraform/modules/cloudfront_cdn/main.tf
@@ -1,8 +1,8 @@
 resource "aws_cloudfront_distribution" "distribution" {
-  enabled             = true
-  is_ipv6_enabled     = true
-  aliases             = [var.cdn_domain_name]
-  price_class         = "PriceClass_100" # North America + Europe only.
+  enabled         = true
+  is_ipv6_enabled = true
+  aliases         = [var.cdn_domain_name]
+  price_class     = "PriceClass_100" # North America + Europe only.
 
   origin {
     domain_name = var.bucket_dns_name
@@ -10,13 +10,15 @@ resource "aws_cloudfront_distribution" "distribution" {
   }
 
   default_cache_behavior {
-    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = var.bucket_origin_id
-    viewer_protocol_policy = "https-only"
-    min_ttl                = 0
-    default_ttl            = 3600  # 1 hour.
-    max_ttl                = 86400 # 1 day.
+    allowed_methods            = ["GET", "HEAD", "OPTIONS"]
+    cached_methods             = ["GET", "HEAD"]
+    target_origin_id           = var.bucket_origin_id
+    viewer_protocol_policy     = "https-only"
+    #origin_request_policy_id   = aws_cloudfront_origin_request_policy.origin_policy.id
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.cors_policy.id
+    min_ttl                    = 0
+    default_ttl                = 3600  # 1 hour.
+    max_ttl                    = 86400 # 1 day.
 
     forwarded_values {
       query_string = false
@@ -35,6 +37,22 @@ resource "aws_cloudfront_distribution" "distribution" {
   viewer_certificate {
     acm_certificate_arn = var.certificate_arn
     ssl_support_method  = "sni-only"
+  }
+}
+
+resource "aws_cloudfront_origin_request_policy" "origin_policy" {
+  name = "origin-policy"
+
+  cookies_config {
+    cookie_behavior = "all"
+  }
+
+  headers_config {
+    header_behavior = "all"
+  }
+
+  query_strings_config {
+    query_string_behavior = "all"
   }
 }
 
