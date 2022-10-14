3025ec184c684c100630e4e734c5630bfa5f3eae
Will Roberts <willroberts3@protonmail.com>
2022-10-14 11:20:44 -0400

    Disable Steam and packaging steps temporarily

--- desktop/gulpfile.babel.js
+++ desktop/gulpfile.babel.js
@@ -12,31 +12,37 @@ function validateConfigForDesktop(cb) {
   return cb();
 }
 
-// Define desktop tasks
 gulp.task('clean:all', clean.all);
+
 desktopPlatforms.forEach((platform) => {
   gulp.task(`desktop:build:${platform}`, (cb) => desktop.build({ platform }, cb));
-  gulp.task(`desktop:build:steam:${platform}`, (cb) => desktop.build({ platform, steam: true }, cb));
-  gulp.task(`desktop:zip:${platform}`, (cb) => desktop.zip(platform, cb));
+  // Temporarily disable Steam & packaging steps.
+  //gulp.task(`desktop:build:steam:${platform}`, (cb) => desktop.build({ platform, steam: true }, cb));
+  //gulp.task(`desktop:zip:${platform}`, (cb) => desktop.zip(platform, cb));
 });
+
 gulp.task('desktop:copy', desktop.copy);
 gulp.task('desktop:yarn', desktop.yarn);
 gulp.task('desktop:shasums', desktop.shasums);
 gulp.task('desktop:setup', desktop.setup);
-//gulp.task('desktop:git:clone', git.desktopClone);
-//gulp.task('desktop:git:sync', git.desktopSync);
-//gulp.task('desktop:git:commit', git.desktopCommit);
-//gulp.task('desktop:git:push', git.desktopPush);
-// gulp.task('desktop:git:diff', git.desktopDiff);
-// gulp.task('desktop:git:publish', git.desktopPublish);
-//gulp.task('desktop:git', gulp.series(
-  //'clean:git',
-  //'desktop:git:clone',
-  //'desktop:git:sync',
-  //'desktop:git:commit',
-  //'desktop:git:push',
-  // 'desktop:git:diff',
-//));
+
+/* Git tasks temporarily disabled.
+gulp.task('desktop:git:clone', git.desktopClone);
+gulp.task('desktop:git:sync', git.desktopSync);
+gulp.task('desktop:git:commit', git.desktopCommit);
+gulp.task('desktop:git:push', git.desktopPush);
+gulp.task('desktop:git:diff', git.desktopDiff);
+gulp.task('desktop:git:publish', git.desktopPublish);
+gulp.task('desktop:git', gulp.series(
+  'clean:git',
+  'desktop:git:clone',
+  'desktop:git:sync',
+  'desktop:git:commit',
+  'desktop:git:push',
+  'desktop:git:diff',
+));
+*/
+
 gulp.task('desktop:build', gulp.series(
   validateConfigForDesktop,
   'clean:all',
@@ -49,29 +55,35 @@ gulp.task('desktop:build', gulp.series(
   'desktop:build:darwin',
   //'desktop:build:win32',
 ));
+
+/* Steam & packaging tasks temporarily disabled.
 gulp.task('desktop:build:steam', gulp.series(
   validateConfigForDesktop,
   'clean:all',
-  //'source',
-  //'rsx:codex_urls',
-  //'rsx:copy',
+  'source',
+  'rsx:codex_urls',
+  'rsx:copy',
   'desktop:setup',
   'desktop:yarn',
   'desktop:copy',
   'desktop:build:steam:darwin',
   'desktop:build:steam:win32',
 ));
+
 gulp.task('desktop:package', gulp.series(
   'desktop:zip:darwin',
   'desktop:zip:win32',
-  //'desktop:git',
+  'desktop:git',
   'desktop:shasums',
-  // 'desktop:git:publish',
+  'desktop:git:publish',
 ));
+
 gulp.task('desktop:package:steam', gulp.series(
   desktop.steamPrep,
   desktop.steamUpload,
 ));
+*/
+
 gulp.task('desktop:build:dev', gulp.series(
   //'rsx:packages',
   //'source',
