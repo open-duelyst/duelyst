aeb1e71ae5cfc83a3d723e4df0aae78d60a15e0d
Will Roberts <willroberts3@protonmail.com>
2022-10-16 12:51:46 -0400

    Create an S3 VPC endpoint

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -19,3 +19,13 @@ resource "aws_route" "default_route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gateway.id
 }
+
+resource "aws_vpc_endpoint" "s3_vpc_endpoint" {
+  vpc_id       = aws_vpc.vpc.id
+  service_name = "com.amazonaws.${var.region}.s3"
+}
+
+resource "aws_vpc_endpoint_route_table_association" "s3_route_table_association" {
+  route_table_id  = aws_vpc.vpc.default_route_table_id
+  vpc_endpoint_id = aws_vpc_endpoint.s3_vpc_endpoint.id
+}
