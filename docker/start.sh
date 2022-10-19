4d68d743e33717ce438add70e16fb3a7b3dbc1a0
Will Roberts <willroberts3@protonmail.com>
2022-10-18 21:08:06 -0400

    Fixes local migrations

--- docker/start.sh
+++ docker/start.sh
@@ -9,7 +9,12 @@ git config --global url."https://".insteadOf git://
 apk add python3 make gcc g++
 
 # Install dependencies.
-yarn install --production && yarn cache clean
+if [ $1 == 'migrate:latest' ]; then
+  # Migrations code uses dev dependencies
+  yarn install --include=dev
+else
+  yarn install --production
+fi
 
 # Use exec to take over the PID from the shell, enabling signal handling.
 exec yarn $1
