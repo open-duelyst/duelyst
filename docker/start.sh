04d8e04a45303fa31c81b9f2b29f057fd0a93a7a
Will Roberts <willroberts3@protonmail.com>
2022-10-25 22:18:54 -0400

    Upgrades from node:16-alpine to node:18-slim

--- docker/start.sh
+++ docker/start.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
+# Install bcrypt dependencies and git.
+# TODO: Isolate bcrypt dependencies to API images only.
+apt-get update && apt-get install -y python3 make gcc g++ git
+
 # Work around boneskull/yargs dependency using the deprecated git protocol.
-apk add git
 git config --global url."https://github.com/".insteadOf git@github.com:
 git config --global url."https://".insteadOf git://
 
-# Install node-gyp dependencies for bcrypt in API and worker
-apk add python3 make gcc g++
-
 # Install dependencies.
-yarn install --production
+yarn install --production && yarn cache clean
 
 # Use exec to take over the PID from the shell, enabling signal handling.
 exec yarn $1
