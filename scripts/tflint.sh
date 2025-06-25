2958bf71b1a03e3b432c619362d92fe289db587a
Will Roberts <willroberts3@protonmail.com>
2025-06-24 21:01:23 -0400

    Updates Terraform linter script

--- scripts/tflint.sh
+++ scripts/tflint.sh
@@ -4,8 +4,8 @@ for dir in $(find terraform | grep '\.tf$' | xargs dirname | sort -u); do
 	if [[ $dir == "terraform/modules"* ]]; then
 		# Ignore provider rules in modules.
 		tflint --disable-rule=terraform_required_providers \
-			--disable-rule=terraform_required_version $dir
+			--disable-rule=terraform_required_version --chdir=$dir
 	else
-		tflint $dir
+		tflint --chdir=$dir
 	fi
 done
